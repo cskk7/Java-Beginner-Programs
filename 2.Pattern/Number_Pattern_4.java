cd0a25df001733fa643b6af82ff11c1a888a9061
C Karthikesavan <karthikesavan2049@gmail.com>
2023-01-10 00:25:29 +0530

    Pattern

---
+++ 2.Pattern/Number_Pattern_4.java
@@ -0,0 +1,22 @@
+/*
+   5 4 3 2 1
+   5 4 3 2
+   5 4 3
+   5 4
+   5
+ */
+
+import java.util.Scanner;
+
+public class Number_Pattern_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = n; j >= i; j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
