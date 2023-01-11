bef9dae66982876797d17c53f842f1396ea13700
C Karthikesavan <karthikesavan2049@gmail.com>
2023-01-11 23:28:54 +0530

    pattern

--- 2.Pattern/Number_Pattern_6.java
+++ 2.Pattern/Number_Pattern_6.java
@@ -1,20 +1,19 @@
 /*
-    * * * * *
-    * * * *
-    * * *
-    * *
-    *
+  5 5 5 5 5
+  4 4 4 4
+  3 3 3
+  2 2
+  1
  */
-
 import java.util.Scanner;
 
-public class Pattern_2 {
+public class Number_Pattern_6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i = n; i >= 1; i--){
-            for(int j = i; j >= 1; j--){
-                System.out.print("* ");
+            for(int j = 1; j <= i; j++){
+                System.out.print(i+" ");
             }
             System.out.println();
         }
--- 2.Pattern/Star_Pattern_2.java
+++ 2.Pattern/Star_Pattern_2.java
@@ -1,23 +1,22 @@
 /*
-    *
-    * *
-    * * *
-    * * * *
     * * * * *
+    * * * *
+    * * *
+    * *
+    *
  */
 
 import java.util.Scanner;
-//pattern_1
-public class Pattern_1{
+
+public class Star_Pattern_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
+        for(int i = n; i >= 1; i--){
+            for(int j = i; j >= 1; j--){
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
-    
 }
