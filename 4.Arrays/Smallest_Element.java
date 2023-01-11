93e7368bace568e1abafbe10bf52a387bb363d05
C Karthikesavan <karthikesavan2049@gmail.com>
2023-01-11 23:36:16 +0530

    first

---
+++ 4.Arrays/Smallest_Element.java
@@ -0,0 +1,22 @@
+/*
+  To find out the SMALLEST ELEMENT inthe array
+ */
+import java.util.Scanner;
+
+public class Smallest_Element {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
