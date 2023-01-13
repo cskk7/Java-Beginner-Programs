a6669d61d87e9e8539afdb1827b8962e985c3274
C Karthikesavan <karthikesavan2049@gmail.com>
2023-01-13 23:32:22 +0530

    first

--- 4.Arrays/ProductOfArray.java
+++ 4.Arrays/ProductOfArray.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class ProductOfArray {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        long product = 1;
+        for(int i = 0; i < n; i++){
+            product *= arr[i];
+        }
+        System.out.println(product);
+    }
 }
