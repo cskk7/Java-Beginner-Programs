450fcf5f25fb1777859cff64e2a90b09e039b819
C Karthikesavan <karthikesavan2049@gmail.com>
2023-11-10 12:30:02 +0530

    fourth

--- 1.Basic Java Programs/CheckPrimeNumber.java
+++ 1.Basic Java Programs/CheckPrimeNumber.java
@@ -9,16 +9,12 @@ class CheckPrimeNumber{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int count =0;
-        for(int i = 2; i < number; i++){
+        int count  = 0;
+        for(int i = 2; i < number/2; i++){
             if(number % i == 0){
                 count++;
             }
         }
-        if(count==0){
-            System.out.println("It is prime number.");
-        }else{
-            System.out.println("It is not a prime number."); 
-        }
+        System.out.println(count == 0 ? "Prime number" : "NOT a prime number");
     }
 }
