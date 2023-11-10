df3812a5ee991bf950d19694ecd19b72b4597a12
C Karthikesavan <karthikesavan2049@gmail.com>
2023-11-10 12:23:18 +0530

    fourth

--- 1.Basic Java Programs/CheckPalindrome.java
+++ 1.Basic Java Programs/CheckPalindrome.java
@@ -7,18 +7,16 @@ import java.util.*;
 public class CheckPalindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        int rev = 0;
+        int number  = sc.nextInt();
+
         int temp = number;
-        while(number > 0){
-            int x = number % 10;
+        int rev = 0;
+        while(number > 0 ){
+            int x = number%10;
             rev = rev*10 + x;
             number /= 10;
         }
-        if(rev==temp){
-            System.out.println("Palindrome Number");
-        }else{
-            System.out.println("Not a Palindrome Number");
-        }
+        System.out.println(rev == temp ? "It is a palindrome" : "It is NOT a palindrome");
+        
     }
 }
